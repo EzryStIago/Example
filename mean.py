eafa28ac58a46a7bcea5d24c8fba16d7cef48975
EzryStIago <ezrysm@gmail.com>
2023-10-25 12:14:13 -0400

    add docstring

--- mean.py
+++ mean.py
@@ -1,6 +1,15 @@
 import numpy as np
 
 def mean(alist):
+    """
+    Calculate the mean value of a given list of numbers.
+
+    Args:
+        alist (list): A list of numbers.
+
+    Returns:
+        float: The mean value of the input list.
+    """
     alist = np.array(alist)
     tot_items = alist.shape[0]
     print(tot_items)
